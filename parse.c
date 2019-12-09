b97a32871501d06aae627ce8fad663f2704d055a
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-12-09 12:10:59 -0500

    parse fixed

--- parse.c
+++ parse.c
@@ -1,11 +1,12 @@
 #include "parse.h"
 
 char ** parse_args(char * line){
-  char ** args = malloc(6 * sizeof(line));
-  char * token;
-  for (int i = 0; token; i++){
+  char ** args = malloc(6 * sizeof(char*));
+  char * token = strsep(&line, " ");
+  int cur_ind = 0;
+  while (token){
+    args[cur_ind] = token;
     token = strsep(&line, " ");
-    args[i] = token;
   }
   return args;
 }
