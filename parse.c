5d2d8127535c55297dfddee13a8f67a634b4877d
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-12-10 09:26:54 -0500

    tried to fix parse function

--- parse.c
+++ parse.c
@@ -6,7 +6,9 @@ char ** parse_args(char * line){
   int cur_ind = 0;
   while (token){
     args[cur_ind] = token;
+    printf("%s\n", token);
     token = strsep(&line, " ");
   }
+  printf("%s %s\n", args[0], args[1]);
   return args;
 }
