f7715bb35c1a9968d6e15d89b57af67c27975955
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-12-10 01:51:33 -0500

    added cd code but not working

--- shell.c
+++ shell.c
@@ -30,21 +30,36 @@ int main(){
     char * s = command;
     
     args = parse_args(s);
+    int len_args = 0;
+    for (int i = 0; !args[len_args]; i ++){
+      len_args++;
+    }
+    len_args++;
 
     if (strcmp(args[0], "exit") == 0){
       exit(0);
     }
 
     else if (strcmp(args[0], "cd")==0){
-      //cd
+      if (len_args >= 2){
+	int fd = chdir(args[1]);
+	if (!fd){
+	  printf("errno: %d, error: %s\n", errno, strerror(errno));
+	}
+      }
     }
 
     else {
-      if (!fork()){
+      int f= fork();
+      if (f) {
+	int status;
+	wait(&status);
+      }
+      else{
 	execvp(args[0], args);
-	free(command);
-	return 0;
       }
     }
+
+    free(command);
   }
 }
